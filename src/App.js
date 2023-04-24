233e06317c2b0c8f91ed5a61d7c44dfb8686495f
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-04-24 16:42:08 +0530

    Added cart context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -13,13 +14,13 @@ function App() {
   }
   const tempCartItem = { id: 'c1', name: 'Sushi', amount: 2, price: 12.99 };
   return (
-    <>
+    <CartProvider>
       {showCart && <Cart cart={[{ ...tempCartItem }]} onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
