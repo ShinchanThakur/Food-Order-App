bd5266ed37027e6a33255338a02a2ab73b1aec3f
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-11-14 19:46:26 +0530

    Changed image location to deploy code

--- src/components/Layout/Background.js
+++ src/components/Layout/Background.js
@@ -1,10 +1,9 @@
-import mealsImage from '../../assets/meals.jpg';
 import styles from './Background.module.css';
 
 const Background = () => {
     return (
         <div className={styles['main-image']}>
-            <img src={mealsImage} alt='A table full of delicious food!' />
+            <img src='/assets/meals.jpg' alt='A table full of delicious food!' />
         </div>
     )
 }
